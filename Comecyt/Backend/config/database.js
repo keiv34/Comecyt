272166baf28b2abd80446340e4397ad18979e840
Ismael Antonio Gonzalez <107378053+keiv34@users.noreply.github.com>
2026-06-28 19:37:00 -0600

    Update database.js

--- Comecyt/Backend/config/database.js
+++ Comecyt/Backend/config/database.js
@@ -8,10 +8,13 @@ const connectionString = process.env.DATABASE_URL || "postgresql://agora_comecyt
 
 // Configuración del pool de PostgreSQL
 const poolConfig = {
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000, // Aumentado a 5s para evitar timeouts en Railway
+  ssl: {
+    rejectUnauthorized: false // <--- Forzamos esto siempre para entornos cloud
+  }
 };
 
 // Usar SSL en entornos externos como Railway o Render
